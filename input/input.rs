8245c4203e83ce9553208269da26319fc04c7e05
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-06-17 13:47:00 -0300

    catching console input in rust

---
+++ input/input.rs
@@ -0,0 +1,9 @@
+use std::io;
+
+fn main() {
+    let mut a =String::new();
+    println!("Enter a string");
+    io::stdin().read_line(&mut a).expect("Failed");
+    let a:String = a.trim().parse().expect("Failed parsing");
+    println!("{} World",a);    
+}
