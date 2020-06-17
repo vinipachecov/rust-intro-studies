27bbfb9644d6ccd5eb22faf8e4a7c76d4b8ada69
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-06-17 13:47:23 -0300

    type-casting or  what is possible with casting

---
+++ typecasting/type-casting.rs
@@ -0,0 +1,19 @@
+
+/**
+ * No typecasting by default
+ *  we use "as" keywor to typecast to another type
+ */
+fn main() {
+    // let a:i32 = 10;
+    // let b:i64 = a as i64;
+    // println!("{}",b);
+
+    // let a:i32 = 10;
+    // let b:i64 = a as i64 + 10;
+    // println!("{}",b);
+
+
+    let a:i32 = 10;    
+    let a:i64 = a as i64 + 10;
+    println!("{}",a);
+}
