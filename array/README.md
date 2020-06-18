e00b23df6505d5c56065be87705bbed4604771ef
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-06-17 21:59:45 -0300

    arrays in rust

---
+++ array/README.md
@@ -0,0 +1,12 @@
+# Array
+- Collection of values
+- All values must be of same type
+- Length is fixed
+
+Syntax:
+```rs
+let a = [1,2,3,5];
+let b: [i32;5] = [1,2,3,4,5];
+let c: [i32;5] = [0;5];
+```
+
---
+++ array/array.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let a: [i32;4] = [22,32,55,66];
+    let mut b: [i32;5] = [0;5];
+    b[2] =5;
+    println!("{:?}",a);
+    println!("{:?}",b);
+    print_array(b);
+
+
+    // Error
+    // let mut c: [i32;5];
+    // c[3] = 5;
+    // println!("{:?}",c);
+}
+
+fn print_array(x: [i32;5]) {
+    println!("x len = {}",x.len());
+    // for n in 0 .. 5 {
+    //     println!("{}", x[n]);
+    // }
+    for n in x.iter() {
+        println!("{}", n);
+    }
+
+    
+}
